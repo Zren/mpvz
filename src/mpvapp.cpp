2e4ea7fa29617443d78b136ec7294cf8f033e670
Chris Holland <zrenfire@gmail.com>
2020-04-10 22:37:34 -0400

    Prep using a Qt 5.15 function to move the window

--- src/mpvapp.cpp
+++ src/mpvapp.cpp
@@ -20,14 +20,15 @@ AppObj::~AppObj() {
 
 void AppObj::dragWindow(QWindow* window) {
 	if (window) {
-		WId windowId = window->winId();
+		xcb_window_t windowId = window->winId();
 
 		QPoint position(0, 0);
 		QPoint rootPosition(0, 0);
 
-		//--- From: BreezeSizeGrip.cpp
+		//--- From: KDE's BreezeSizeGrip.cpp
 		// button release event
 		auto connection( QX11Info::connection() );
+
 		xcb_button_release_event_t releaseEvent;
 		memset(&releaseEvent, 0, sizeof(releaseEvent));
 
@@ -47,6 +48,76 @@ void AppObj::dragWindow(QWindow* window) {
 
 		xcb_ungrab_pointer( connection, XCB_TIME_CURRENT_TIME );
 		//---
+
+		//--- From: KDE's XWindowTasksModel::requestMove
+		// https://github.com/KDE/plasma-workspace/blob/c06d8975d5bf9032c2c1e4050468cfc19891fffe/libtaskmanager/xwindowtasksmodel.cpp#L784
+		// https://github.com/KDE/kwindowsystem/blob/master/src/platforms/xcb/netwm.cpp
+		// https://github.com/KDE/kwindowsystem/blob/d3bc79da92564bfda0337f16ad5c2f72c0327edf/src/platforms/xcb/netwm.cpp#L1583
+		// https://github.com/KDE/kwindowsystem/blob/d3bc79da92564bfda0337f16ad5c2f72c0327edf/src/platforms/xcb/netwm.cpp#L361
+		
+		// NETRootInfo ri(QX11Info::connection(), NET::WMMoveResize);
+		// ri.moveResizeRequest(window, geom.center().x(), geom.center().y(), NET::Move);
+
+		// mask = netwm_sendevent_mask;
+		// xcb_window_t destination = windowId; // root
+		// xcb_window_t window = windowId;
+		// message = p->atom(_NET_WM_MOVERESIZE);
+		// const uint32_t data[5] = {
+		// 	uint32_t(x_root),
+		// 	uint32_t(y_root),
+		// 	uint32_t(direction),
+		// 	0,
+		// 	0
+		// };
+
+		// xcb_client_message_event_t event;
+		// event.response_type = XCB_CLIENT_MESSAGE;
+		// event.format = 32;
+		// event.sequence = 0;
+		// event.window = window;
+		// event.type = message;
+		// for (int i = 0; i < 5; i++) {
+		// 	event.data.data32[i] = data[i];
+		// }
+		// xcb_send_event( connection, false, windowId, mask, (const char *) &event );
+
+		//---
+		// https://code.qt.io/cgit/qt/qtbase.git/tree/src/plugins/platforms/xcb/qxcbwindow.cpp?h=dev#n2374
+		// QPoint globalPos(0, 0);
+
+		// const xcb_atom_t moveResize = connection->atom(QXcbAtom::_NET_WM_MOVERESIZE);
+		// xcb_client_message_event_t xev;
+		// xev.response_type = XCB_CLIENT_MESSAGE;
+		// xev.type = moveResize;
+		// xev.sequence = 0;
+		// xev.window = xcb_window();
+		// xev.format = 32;
+		// xev.data.data32[0] = globalPos.x();
+		// xev.data.data32[1] = globalPos.y();
+		// xev.data.data32[2] = 8; // move
+		// xev.data.data32[3] = XCB_BUTTON_INDEX_1;
+		// xev.data.data32[4] = 0;
+		// xcb_ungrab_pointer(connection->xcb_connection(), XCB_CURRENT_TIME);
+		// xcb_send_event(
+		// 	connection->xcb_connection(),
+		// 	false,
+		// 	xcb_window(), // xcbScreen()->root(),
+		// 	XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY,
+		// 	(const char *)&xev
+		// );
+
+		//---
+
+
+		//---
+		// https://phabricator.kde.org/D11573
+		// https://codereview.qt-project.org/c/qt/qtbase/+/219277
+		// TODO: Qt 5.15
+
+		// QPlatformWindow *platformWindow = window->handle();
+		// if (platformWindow) {
+		// 	platformWindow->startSystemMove(window->mapFromGlobal(position));
+		// }
 	}
 }
 
