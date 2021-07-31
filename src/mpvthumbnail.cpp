3fbd07fcb5aa566b284a57e668daf25557545bde
Chris Holland <zrenfire@gmail.com>
2021-07-31 16:48:38 -0400

    Add thumbnail mpv instance options used by bomi.

--- src/mpvthumbnail.cpp
+++ src/mpvthumbnail.cpp
@@ -12,11 +12,26 @@ MpvThumbnail::MpvThumbnail(QQuickItem *parent)
 	: MpvObject(parent)
 {
 	setEnableAudio(false);
+	setOption("hwdec", "no");
+	setOption("aid", "no"); // No audio track
 	setOption("sid", "no"); // Hide subs
 	setOption("hr-seek", "no"); // Don't use precise seek
 	setOption("sstep", "10"); // Step every 10 seconds
-	setOption("pause", ""); // Init paused
+	setOption("pause", "yes"); // Init paused
 	setOption("really-quiet", "yes"); // No stdout
+
+	// From bomi
+	// https://github.com/ashinan/bomi/blob/master/src/bomi/video/videopreview.cpp#L32
+	setOption("audio-file-auto", "no");
+	setOption("sub-auto", "no");
+	setOption("osd-level", "0");
+	// setOption("quiet", "yes");
+	setOption("title", "\"\"");
+	setOption("audio-pitch-correction", "no");
+	setOption("keep-open", "always");
+	setOption("vd-lavc-skiploopfilter", "all"); // Skips the loop filter (AKA deblocking) during H.264 decoding.
+	setOption("use-text-osd", "no");
+	setOption("audio-display", "no");
 }
 
 MpvThumbnail::~MpvThumbnail() {
